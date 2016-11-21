f9d0515ee75b736e5c0de4d1f798e09199f652e4
harold <harold.trannois@u-picardie.fr>
2016-11-21 11:46:42 +0100

    Exo n° 5 Premier rendu

---
+++ public/exoView.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+$vm = new \Zend\View\Model\ViewModel();
+$vm->setVariables( array('nom' => 'tintin'));
+$vm->setTemplate( 'liste' );
+
+$resolv = new \Zend\View\Resolver\TemplateMapResolver( [ 'liste' => __DIR__.'/../src/view/template/layout.phtml']);
+
+$rendu = new \Zend\View\Renderer\PhpRenderer();
+$rendu->setResolver($resolv);
+echo $rendu->render( $vm );
